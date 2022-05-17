7208eda81c8d32e0e567835399521083e87ad920
DanielRovira <danielroviratorres@gmail.com>
2022-05-17 23:19:00 +0100

    flexivel

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,24 @@
 
 const personagens = document.querySelectorAll('.personagem')
-
+document.getElementById('nome-jogador-1').textContent = char1
+document.getElementById('nome-jogador-2').textContent = char6
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
         const idSelecionado = personagem.attributes.id.value;
-        
-
-        const personagemSelecionado = document.querySelector('.selecionado')
+        const personagemSelecionado = document.querySelector('.selecionado');
         const imagemJogador1 = document.getElementById('personagem-jogador-1');
         const nomeJogador1 = document.getElementById('nome-jogador-1');
-        const dataName = personagem.getAttribute('data-name')
-        // if(idSelecionado === 'ultron') return;   // Desativar selecionar mesmo personagem que Player 2
-        nomeJogador1.textContent = dataName;
+        idSelecionado === "char1" ? nomeJogador1.textContent = char1 : 
+        (idSelecionado === "char2" ? nomeJogador1.textContent = char2 : 
+        (idSelecionado === "char3" ? nomeJogador1.textContent = char3 : 
+        (idSelecionado === "char4" ? nomeJogador1.textContent = char4 : 
+        (idSelecionado === "char5" ? nomeJogador1.textContent = char5 : 
+        (idSelecionado === "char6" ? nomeJogador1.textContent = char6 : 
+        (idSelecionado === "char7" ? nomeJogador1.textContent = char7 : 
+        (idSelecionado === "char8" ? nomeJogador1.textContent = char8 : 
+        nomeJogador1.textContent = char9)))))));
         imagemJogador1.src = `./src/imagens/${idSelecionado}.png`;
         personagemSelecionado.classList.remove('selecionado')
         personagem.classList.add('selecionado')
     });
-    
 });
-
---
+++ src/js/nomes.js
@@ -0,0 +1,10 @@
+
+ var char1 = "Thor";
+ var char2 = "Homem de ferro";
+ var char3 = "Viúva Negra";
+ var char4 = "Hulk";
+ var char5 = "Capitão América";
+ var char6 = "Ultron";
+ var char7 = "Doutor Doom";
+ var char8 = "Nova";
+ var char9 = "Fênix";
